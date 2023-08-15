9cca7ae09a8af364017272de4ec49fb87e78eaa1
Sagar Dhanaraj Mahajan <102789884+SagarSuryawanshiGujar@users.noreply.github.com>
2023-08-15 15:26:24 +0530

    Added README file

--- script.js
+++ script.js
@@ -93,24 +93,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-/*
-
-     The Complete JavaScript Course 14
-
-JavaScript in the Browser: DOM and Events
-
-                  Coding Challenge #1
-Implement a game rest functionality, so that the player can make a new guess!
-Your tasks: 
-
-1. Select the element with the 'again' class and attach a click event handler
-
-2. In the handler function, restore initial values of the 'score' and 'secretNumber' variables
-
-3. Restore the initial conditions of the message, number, score and guess input fields
-
-4. Also restore the original background color (#222) and number width (15rem)
-
-GOOD LUCK 😀
-
-*/
